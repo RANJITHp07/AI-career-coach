dcd9f737719667d8c0edf4c954c39eda1cbf085d
ranjith <ranjithp27122003@gmail.com>
2025-06-21 13:30:58 +0530

    chore(client) : completed base setup of the client along with the shadcn setup

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="p-2">
+      <p className="text-xl text-white">HIii</p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
