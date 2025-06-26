d3eb459c06f623e16de6054e953261c83ba06c3a
Ranjithp07 <ranjithp27122003@gmail.com>
2025-06-26 14:02:25 +0530

    chore(validator-package): completed the base setup and added registration validator

---
+++ packages/validator/src/userValidation/registration.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const SignInMethodEnum = z.enum(["GOOGLE", "GITHUB", "EMAIL"]);
+
+export const CreateUserSchema = z.object({
+    id: z.string(),
+    clerkUserId: z.string(),
+    email: z.string().email(),
+    name: z.string().optional(),
+    password: z.string().min(6).optional(),
+    imageUrl: z.string().url().optional(),
+    signInMethod: SignInMethodEnum
+});
+
+export type TCreateUserInput = z.infer<typeof CreateUserSchema>;
